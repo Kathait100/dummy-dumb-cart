1c5d763009573e0e2761395b52d39550b544e1bf
Kathait100 <82964553+Kathait100@users.noreply.github.com>
2021-09-25 09:58:05 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Home from "./home/home";
+import Navbar from "./navbar/navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
